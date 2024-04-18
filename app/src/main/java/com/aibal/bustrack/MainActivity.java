e505081c37188a09ff76a6225f894af7e7fa3379
SHNR <34773774+ShaunRodrigues@users.noreply.github.com>
2024-04-18 19:39:39 +0530

    alx

--- app/src/main/java/com/aibal/bustrack/MainActivity.java
+++ app/src/main/java/com/aibal/bustrack/MainActivity.java
@@ -15,8 +15,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button si = findViewById(R.id.button);
         Button ab = findViewById(R.id.button2);
+        Button sch = findViewById(R.id.button3);
+        Button pro = findViewById(R.id.button4);
         Intent a = new Intent(this, MapsActivity.class);
         Intent b = new Intent(this, About.class );
+        Intent c = new Intent(this, Detail.class);
+        Intent d = new Intent(this, Profile.class );
         si.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,6 +31,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(b);
+            }
+        });
+        sch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(c);
+            }
+        });
+        pro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(d);
             }
         });
     }
